06c5415975ba81eb548429bc6aa8bf47725ef0f0
Heliosmaster <helios89@gmail.com>
2013-02-23 14:08:29 +0100

    added localview

---
+++ localview.m
@@ -0,0 +1,48 @@
+% A = sparse matrix with values either 1 or 2
+% TODO: construction of sparse matrices by indexes and 'sparse' to speed up
+%       breaking ties randomly (for the 1st case)?
+
+function [Bc,Br]=localview(A)
+    Br = sparse(0,0);
+    Bc = sparse(0,0);
+    [m,n] = size(A);
+    [i1,j1,s1] = find(A==1);
+    A1 = sparse(i1,j1,s1,m,n);
+    [i2,j2,s2] = find(A==2);
+    A2 = sparse(i2,j2,s2,m,n);
+    [I,J] = find(A);
+    %spy(A1,'r'); hold on;
+    %spy(A2,'g'); hold off;
+    
+    len = length(I);
+    while(len>0) 
+        k = randi([1,len]);
+        i = I(k);
+        j = J(k);
+        %fprintf('picked (%g,%g) - ',i,j);
+        rowsplit = (nnz(A1(i,:))) && (nnz(A2(i,:)));
+        colsplit = (nnz(A1(:,j))) && (nnz(A2(:,j)));
+        if (~xor(rowsplit,colsplit))
+            if nnz(A(i,:)) < nnz(A(:,j))
+                Br(i,j) = 1;
+                %fprintf('row!\n');
+            else
+                Bc(i,j) = 1;
+                %fprintf('col!\n');
+            end
+        else
+            if rowsplit
+                Bc(i,j) = 1;
+                %fprintf('col!\n');
+            else
+                Br(i,j) = 1;
+                %fprintf('row!\n');
+            end
+        end
+        I(k) = [];
+        J(k) = [];
+        len = len-1;
+    end
+end
+    
+    
---
+++ test_mat.m
@@ -0,0 +1,3 @@
+clear all; close all;
+A=[2 2 0 0 0; 0 1 1 0 0; 0 0 1 0 0; 0 0 0 2 0; 1 0 0 0 2];
+A=sparse(A);
