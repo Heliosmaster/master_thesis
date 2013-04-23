113c141295ee99fbaab6d07009dbc5221358ce4d
Heliosmaster <helios89@gmail.com>
2013-04-23 15:35:30 +0200

    fullsort now is simply sort

--- hot_restart/phase_1/pa_fullsort_simple.m
+++
@@ -1,7 +0,0 @@
-function [v,l]=pa_fullsort_simple(A)
-[uncut,cut]=uncut_before_cut(A);
-l = length(uncut);
-nz=get_nnz(A);
-[~,iu] = sort(nz(uncut));
-[~,ic] = sort(nz(cut));
-v = [uncut(iu) cut(ic)];
--- hot_restart/phase_1/po_fullsort_simple.m
+++
@@ -1,8 +0,0 @@
-function v=po_fullsort_simple(A,varargin)
-if nargin < 2
-    ord = 'ascend';
-else
-    ord = varargin{1};
-end
-nz=get_nnz(A);
-[~,v] = sort(nz,ord);
