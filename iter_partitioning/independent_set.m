110e8ea160dc4bc45c07962eea541c0fa7807217
Heliosmaster <helios89@gmail.com>
2013-07-04 12:05:59 +0200

    fixed output creation

--- iter_partitioning/independent_set.m
+++ iter_partitioning/independent_set.m
@@ -1,6 +1,11 @@
 function v=independent_set(matrix,I)
 [uncut,cut]=uncut_before_cut(I);
-dlmwrite('matlab_temp.txt',uncut,'\n');
+%dlmwrite('matlab_temp.txt',uncut,'\n');
+fid = fopen('matlab_temp.txt','w');
+for i=1:size(uncut,2)
+  fprintf(fid,'%g\n',uncut(i));
+end
+fclose(fid);
 python_cmd = ['python maximum_independent_set.py ' matrix];
 unix(python_cmd);
 delete('matlab_temp.txt');
