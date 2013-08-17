ca26705553d66ef9578adb4f2c391eda9a1e17bc
Heliosmaster <helios89@gmail.com>
2013-08-17 16:37:44 +0200

    batch script and processing of hot_restart results

--- iter_partitioning/hot_restart/batch.sh
+++ iter_partitioning/hot_restart/batch.sh
@@ -1,21 +1,33 @@
 #! /bin/bash
 
-matrix="../../matrices/$1"
-matrix_name=${1%.*}
-output_dir="../../logs/hr/$matrix_name"
-if [ ! -d $output_dir ]; then
-	mkdir $output_dir
+# grep average * | sed -r 's/hr-([0-9][0-9]?-[0-1]-[0-1])\.log:average initials\s+([0-9]\.[0-9][0-9])\s+average finals:\s+([0-9]\.[0-9][0-9])/\1: \2 \3/g'
+
+matrix_dir="../../matrices/"
+results_dir="../../logs/hr_summary"
+if [ ! -d $results_dir]; then
+	mkdir $results_dir
 fi
-echo $matrix
-for i in {1..11}
+for matrix in $matrix_dir*
+do
+	matrix_name=$(basename "$matrix")
+	matrix_name=${matrix_name%.*}
+	output_dir="../../logs/hr/$matrix_name"
+	echo $matrix_name
+	if [ ! -d $output_dir ]; then
+		mkdir $output_dir
+	fi
+	for i in {1..11}
 	do
 		for j in 0 1
 		do
 			for k in 0 1
 			do
-				echo "Doing $i $j $k"
+				echo -e "  $i $j $k"
 				./hot_restart $matrix $i $j $k &> $output_dir/hr-$i-$j-$k.log
 			done
 		done
-#	./hot_restart $
+	done
+	$(grep average ../../logs/hr/$matrix_name/* | sed -r 's/.*hr-([0-9][0-9]?-[0-1]-[0-1])\.log:average initials\s+([0-9]\.[0-9][0-9])\s+average finals:\s+([0-9]\.[0-9][0-9])/\1: \2 \3/g'>> $results_dir/$matrix_name.txt)
+
+
 done
